50606b28c569bd05e4432f0c0709ded588a8a58b
xgheter <viniciusjesusfilho09@gmail.com>
2024-03-13 18:13:27 -0300

    feat: Adição da página de Sobre

---
+++ Notes/Notes/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+using Windows.Foundation;
+
+namespace Notes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void LearnMore_Clicked(object sender, EventArgs e)
+	{
+		await Launcher.OpenAsync("https://www.google.com");
+	}
+}
